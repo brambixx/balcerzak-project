b49e19bd0c75a892aafc1b38cc07a3a1dcd3fefd
brambixx <kubabrambor97@o2.pl>
2021-05-24 14:29:07 +0200

    Add article pages and mobile menu

--- footer.php
+++ footer.php
@@ -1,1 +1,2 @@
+<script src="<?php echo get_template_directory_uri().'/assets/js/main.js'?>"></script>
 <?php echo "Footer :)"?>
--- single-post.php
+++ single-post.php
@@ -8,8 +8,17 @@
 ?>
 <body>
 <?php get_template_part('template-parts/navbar'); ?>
-    <h1>
-        <?php the_content();?>
-    </h1>
+    <div class="container">
+        <h1><?php the_title();?></h1>
+        <div class="article__text">
+            <?php the_content();?>
+        </div>
+    </div>
+    <div class="container">
+        <?php the_post_thumbnail()?>
+    <?php previous_post_link( '%link', 'Poprzedni artykuł', TRUE, '13' ); ?>
+    <?php next_post_link( '%link', 'Następny artykuł', TRUE ); ?>
+    </div>
+<?php get_footer();?>
 </body>
 </html>
